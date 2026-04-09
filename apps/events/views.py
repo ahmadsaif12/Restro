48b4b41b16757091ddbe4d71ee418e972e0816d0
ahmadsaif12 <ahmads87237@gmail.com>
2026-04-09 10:58:36 +0545

    Added modules events

--- apps/events/views.py
+++ apps/events/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from django.utils import timezone
+from apps.events.models import Events
+from apps.events.serializers import EventSerializer
 
-# Create your views here.
+class EventViewSet(viewsets.ModelViewSet):
+    serializer_class = EventSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = Events.objects.all().order_by("date", "time")
+        filter_type = self.request.query_params.get("filter", "all")
+        today = timezone.localdate()
+
+        if filter_type == "today":
+            queryset = queryset.filter(date=today)
+        elif filter_type == "upcoming":
+            queryset = queryset.filter(date__gt=today)
+
+        return queryset
+
+    def perform_create(self, serializer):
+        serializer.save()
