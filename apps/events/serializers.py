9fd899d21266afb1e79b47fd65f75e9abd881b9c
ahmadsaif12 <ahmads87237@gmail.com>
2026-04-09 11:04:17 +0545

    updated

--- apps/events/serializers.py
+++ apps/events/serializers.py
@@ -1,8 +1,21 @@
 from rest_framework import serializers
+from django.utils import timezone
 from apps.events.models import Events
 
-
 class EventSerializer(serializers.ModelSerializer):
+    local_date = serializers.SerializerMethodField()
+    local_time = serializers.SerializerMethodField()
+
     class Meta:
         model = Events
         fields = "__all__"
+
+    def get_local_date(self, obj):
+        if obj.date:
+            return timezone.localtime(timezone.make_aware(timezone.datetime.combine(obj.date, timezone.datetime.min.time()))).date()
+        return None
+
+    def get_local_time(self, obj):
+        if obj.time:
+            return obj.time  
+        return None
